dbb328641e2c23a78d68624cd3b6e539780c4d5a
Nilesh Thakur <nileshthakurbgp@gmail.com>
2025-10-25 11:24:43 +0530

    added membership functionality

---
+++ app/club/community/member-verification/members.tsx
@@ -0,0 +1,156 @@
+export const memberData = [
+  {
+    "id": "GCC-M1A5J0X3K9Z",
+    "regNo": "TBD",
+    "name": "TBD",
+    "role": "TBD",
+    "initials": "TBD",
+    "github": "TBD",
+    "linkedin": "TBD",
+    "email": "TBD",
+    "joinDate": "TBD"
+  },
+  {
+    "id": "GCC-M2B6K1Y4L0A",
+    "regNo": "TBD",
+    "name": "TBD",
+    "role": "TBD",
+    "initials": "TBD",
+    "github": "TBD",
+    "linkedin": "TBD",
+    "email": "TBD",
+    "joinDate": "TBD"
+  },
+  {
+    "id": "GCC-M3C7L2Z5M1B",
+    "regNo": "TBD",
+    "name": "TBD",
+    "role": "TBD",
+    "initials": "TBD",
+    "github": "TBD",
+    "linkedin": "TBD",
+    "email": "TBD",
+    "joinDate": "TBD"
+  },
+  {
+    "id": "GCC-M4D8M3A6N2C",
+    "regNo": "TBD",
+    "name": "TBD",
+    "role": "TBD",
+    "initials": "TBD",
+    "github": "TBD",
+    "linkedin": "TBD",
+    "email": "TBD",
+    "joinDate": "TBD"
+  },
+  {
+    "id": "GCC-M5E9N4B7O3D",
+    "regNo": "TBD",
+    "name": "TBD",
+    "role": "TBD",
+    "initials": "TBD",
+    "github": "TBD",
+    "linkedin": "TBD",
+    "email": "TBD",
+    "joinDate": "TBD"
+  },
+  {
+    "id": "GCC-M6F0O5C8P4E",
+    "regNo": "TBD",
+    "name": "TBD",
+    "role": "TBD",
+    "initials": "TBD",
+    "github": "TBD",
+    "linkedin": "TBD",
+    "email": "TBD",
+    "joinDate": "TBD"
+  },
+  {
+    "id": "GCC-M7G1P6D9Q5F",
+    "regNo": "23105129022",
+    "name": "Harshdeep",
+    "role": "UI/UX Designer",
+    "initials": "H",
+    "github": "TBD",
+    "linkedin": "TBD",
+    "email": "harshdeep7858@gmail.com",
+    "joinDate": "Oct 2025"
+  },
+  {
+    "id": "GCC-M8H2Q7E0R6G",
+    "regNo": "23105129029",
+    "name": "Harshita Kusum",
+    "role": "UI/UX Designer",
+    "initials": "HK",
+    "github": "TBD",
+    "linkedin": "TBD",
+    "email": "harshitakusum211@gmail.com",
+    "joinDate": "Oct 2025"
+  },
+  {
+    "id": "GCC-M9I3R8F1S7H",
+    "regNo": "23105129060",
+    "name": "Amol Pratap",
+    "role": "Project Developer",
+    "initials": "AP",
+    "github": "TBD",
+    "linkedin": "TBD",
+    "email": "amolapratap@gmail.com",
+    "joinDate": "Oct 2025"
+  },
+  {
+    "id": "GCC-M0J4S9G2T8I",
+    "regNo": "24105129902",
+    "name": "Aashka Jaiswal",
+    "role": "Project Developer",
+    "initials": "AJ",
+    "github": "TBD",
+    "linkedin": "TBD",
+    "email": "jaiswalashka@gmail.com",
+    "joinDate": "Oct 2025"
+  },
+  {
+    "id": "GCC-M1K5T0H3U9J",
+    "regNo": "23105129011",
+    "name": "Ashutosh Raj",
+    "role": "Social Media and Event Manager",
+    "initials": "AR",
+    "github": "TBD",
+    "linkedin": "TBD",
+    "email": "ashu953442@gmail.com",
+    "joinDate": "Oct 2025"
+  },
+  {
+    "id": "GCC-M2L6U1I4V0K",
+    "regNo": "23105129006",
+    "name": "Priti Kumari",
+    "role": "Project Developer",
+    "initials": "PK",
+    "github": "TBD",
+    "linkedin": "TBD",
+    "email": "pm5196521@gmail.com",
+    "joinDate": "Oct 2025"
+  },
+  {
+    "id": "GCC-M3M7V2J5W1L",
+    "regNo": "23105129062",
+    "name": "Dhiraj Kumar",
+    "role": "Project Developer",
+    "initials": "DK",
+    "github": "TBD",
+    "linkedin": "TBD",
+    "email": "drai6186@gmail.com",
+    "joinDate": "Oct 2025"
+  },
+  {
+    "id": "GCC-M4N8W3K6X2M",
+    "regNo": "23105129102",
+    "name": "Vikash Kumar",
+    "role": "Project Developer",
+    "initials": "VK",
+    "github": "TBD",
+    "linkedin": "TBD",
+    "email": "kumarvikash78588@gmail.com",
+    "joinDate": "Oct 2025"
+  }
+]
